2e357f1eb2ff8ef0635ac01be7f29e26f860da79
Samantha Callie <Hoomee90@gmail.com>
2024-11-19 15:46:04 -0800

    add post column component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PostsColumn from "./PostsColumn";
+
+function App() {
+  return (
+    <React.Fragment>
+      <PostsColumn />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/PostsColumn.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function PostsColumn() {
+  return (
+    <h2>Posts</h2>
+  );
+}
+
+export default PostsColumn;
