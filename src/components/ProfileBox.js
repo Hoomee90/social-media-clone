33162a3f4021c39def0aa53c2ffd5da186d2800d
Samantha Callie <Hoomee90@gmail.com>
2024-12-03 14:19:20 -0800

    add profile box links

--- src/components/ProfileBox.js
+++ src/components/ProfileBox.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Stack } from "react-bootstrap";
 import Card from 'react-bootstrap/Card';
 
 function ProfileBox() {
@@ -7,7 +8,15 @@ function ProfileBox() {
       <Card>
         <Card.Header className="p-5 rounded-0" style={{ backgroundColor: "#3498DB" }}></Card.Header>
         <Card border="light" className="z-1 position-absolute profile-picture" style={{ backgroundColor: "#3498DB" }}></Card>
-        <Card.Title className="py-1 me-3 text-end" >Placeholder</Card.Title>
+        <Card.Title className="pt-1 me-3 text-end" >Kellie A. Corrigan</Card.Title>
+        <Card.Body>
+          <Stack direction="horizontal" gap={4}>
+            <a href="#">POSTS</a>
+            <a href="#">FOLLOWING</a>
+            <a href="#">FOLOLOWERS</a>
+          </Stack>
+        </Card.Body>
+
       </Card>
     </React.Fragment>
   );
