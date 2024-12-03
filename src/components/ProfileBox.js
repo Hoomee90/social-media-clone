7511e12ce08a15ccadb5a0f3effde44d1c018b62
Samantha Callie <Hoomee90@gmail.com>
2024-12-03 13:59:04 -0800

    perfectly position profile picture

--- src/components/ProfileBox.js
+++ src/components/ProfileBox.js
@@ -6,10 +6,8 @@ function ProfileBox() {
     <React.Fragment>
       <Card>
         <Card.Header className="p-5 rounded-0" style={{ backgroundColor: "#3498DB" }}></Card.Header>
-        <Card border="light" className="p-5 position-absolute bottom-50 end-50" style={{ backgroundColor: "#3498DB" }}></Card>
-        <Card.Body>
-          <Card.Title className="pb-2" >Placeholder</Card.Title>
-        </Card.Body>
+        <Card border="light" className="z-1 position-absolute profile-picture" style={{ backgroundColor: "#3498DB" }}></Card>
+        <Card.Title className="py-1 me-3 text-end" >Placeholder</Card.Title>
       </Card>
     </React.Fragment>
   );
